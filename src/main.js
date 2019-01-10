c32c8451e8d387b7542ed8d4f3ada2c50d3419e6
AliceInWonderdev <ali.castaneda05@gmail.com>
2019-01-10 19:47:40 -0300

    Main.j: upgrade the code of DOM

--- src/main.js
+++ src/main.js
@@ -16,93 +16,95 @@ submit.addEventListener("click",()=>{
   let sortOrder = "asc"
   let data = pokemon["pokemon"]
   let result = sortPokemonBy(data, sortBy, sortOrder)
-
-  
-
+  renderPokemonListInView(result)
   });
 
+//para mostrar tarjetas con bootstrap
+const card = "";
+const containterRoot = document.getElementById("root");
 
-
-
+const showData = (data) =>{
+  
+  data.forEach(element => {
+  card = containterRoot.innerHTML += `
+  <div class="row">
+    <div class="card col-4">
+      <div class="box">
+        <div class="img">
+            <img src="${element.img}">
+        </div>
+        <h2>${element.name}</h2>
+        <p>${element.type}</p>
+      </div>
+    </div>
+  </div>`
+  });
+  console.log("hola");
+  return card;
+};
+showData(data)
 
 //función que carga la lista de todos los pokemones
 function renderPokemonListInView(allPokemon){
   let pokemonListContainer = document.getElementById("pokemonList");
   pokemonListContainer.innerHTML= "";
 
+
   allPokemon.forEach( pokemon =>{
     let parent = document.createElement("div");
-    parent.className = "pokemonItem"
+      parent.className = "pokemonItem"
+
+//image
+//creamos un elemento IMG (porque es una imagen) y la metemos en la variable pokemonImage
+    let pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemon.img;
+    pokemonImage.className = "pokemonImage";
+    parent.appendChild(pokemonImage);
+
+    parent.appendChild(createClear());
+
+//Number
+    let pokemonNum = document.createElement("span");
+    pokemonNum.innerHTML = pokemon.num;
+    pokemonNum.className = "pokemonNum";
+    parent.appendChild(pokemonNum);
+
+    parent.appendChild(createClear());
+
+//name. 
+//Span es para poner un texto dentro del div papá. 
+//al pokemonName le digo que ponga un HTML dentro de él.
+//a  la variable creada le asigno una clase para identificarla(le asigné el mismo nombre para identificar mejor).
+//a la variable creada le asigno un papá que será el parent.
+    let pokemonName = document.createElement("span");
+    pokemonName.innerHTML = pokemon.name;
+    pokemonName.className = "pokemonName";
+    parent.appendChild(pokemonName);
+
+    parent.appendChild(createClear());
 
 
-  
-     //image
-      //creamos un elemento IMG (porque es una imagen) y la metemos en la variable pokemonImage
-      let pokemonImage = document.createElement("img");
-      pokemonImage.src = pokemon.img;
-      pokemonImage.className = "pokemonImage";
-      parent.appendChild(pokemonImage);
+//type
+let pokemonType = document.createElement("span");
+  pokemonType.innerHTML = pokemon.type;
+  pokemonType.classList = "pokemonType";
+  parent.appendChild( pokemonType );
+  pokemonListContainer.appendChild(parent)
+   
+  });
 
-      parent.appendChild(createClear());
+};
 
-      //Number
-       let pokemonNum = document.createElement("span");
-       pokemonNum.innerHTML = pokemon.num;
-       pokemonNum.className = "pokemonNum";
-       parent.appendChild(pokemonNum);
-       parent.appendChild(createClear());
-     //name. 
-      //Span es para poner un texto dentro del div papá. 
-      //al pokemonName le digo que ponga un HTML dentro de él.
-      //a  la variable creada le asigno una clase para identificarla(le asigné el mismo nombre para identificar mejor).
-      //a la variable creada le asigno un papá que será el parent.
-      let pokemonName = document.createElement("span");
-      pokemonName.innerHTML = pokemon.name;
-      pokemonName.className = "pokemonName";
-      parent.appendChild(pokemonName);
-      parent.appendChild(createClear());
-
-      /*/Weaknesses
-      let pokemonWeaknesses = pokemon.weaknesses
-      for (i = 0; i < pokemonWeaknesses.length; i++){
-        let pokemonWeak = document.createElement("span");
-        pokemonWeak.innerHTML = pokemonWeaknesses[i]+" ";
-        parent.appendChild(pokemonWeak);
+/*/Weaknesses
+let pokemonWeaknesses = pokemon.weaknesses
+for (i = 0; i < pokemonWeaknesses.length; i++){
+let pokemonWeak = document.createElement("span");
+pokemonWeak.innerHTML = pokemonWeaknesses[i]+" ";
+parent.appendChild(pokemonWeak);
       }
 
-      parent.appendChild(createClear());*/
+ parent.appendChild(createClear());*/
       
-      //type
-      let pokemonType = document.createElement("span");
-      pokemonType.innerHTML = pokemon.type;
-      pokemonType.classList = "pokemonType";
-      parent.appendChild( pokemonType );
-      pokemonListContainer.appendChild(parent)
-       
-  });
-
-}
-
-/*/ función que ordena
-sortPokemonBy(data, sortBy, sortOrder) => {
-  let pokeSort = data;
-  pokeSort.sort((a,b) => {
-    if (sortBy === "key" && sortOrder === "ascending"){
-      console.log(pokeSort)
-    }
-  })
-}
-document.getElementById("sortPokemon").innerHTML = pokemon["pokemon"];
-function sortPokemonBy(){
-  pokemon["pokemon"].sort();
-  document.getElementById("sortPokemon").innerHTML = pokemon["pokemon"];
-}
-function sortPokemonBy(){
-  var numbers = pokemon["pokemon"].num
-  numbers.sort(function(a,b){
-    return a-b
-  });
-}*/
 
 
 
@@ -112,5 +114,28 @@ function createClear() {
   return clear
 }
 
+//charts
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
 
 
