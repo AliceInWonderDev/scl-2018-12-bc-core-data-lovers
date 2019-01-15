7242a396f04e8955f8eef7407b896bdb0a7a58b7
AliceInWonderdev <ali.castaneda05@gmail.com>
2019-01-15 14:56:10 -0300

    Main.js: adding comments to the DOM functions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 window.loadAllPokemon()
 function loadAllPokemon(){
 const allPokemon = pokemon["pokemon"];
+//Con el render puedo mostrar la lista de todos los Pokémons
 renderPokemonListInView(allPokemon);
 }
 //variable para utilizar con el filtro
 const text = document.getElementById("searchPokemon");
+
 //variable para utilizarla con la función ordenar
 const submit = document.getElementById("myCheck");
+//agregué un addEventListener para que al hacer clic en el boton de a-z pudiera ejecutarse y mostrarse lo solicitado
 submit.addEventListener("click",()=>{
 let sortBy = "name"
 let sortOrder = "asc"
@@ -14,9 +17,10 @@ let data = pokemon["pokemon"]
 let result = sortPokemonBy(data, sortBy, sortOrder)
 renderPokemonListInView(result)
 });
-//para mostrar tarjetas con bootstrap
+
 //función que carga la lista de todos los pokemones
 function renderPokemonListInView( allPokemon ){
+
 //para agarrar el calculo
 let finalValue = `
 <div class="container">
@@ -27,23 +31,23 @@ let finalValue = `
    </div>
 </div>
 `
+//DOM para mostrar la cuenta de los Pokémones
 document.getElementById("pokeCounter").innerHTML = finalValue
+//Ordenando las cards como quiero que se muestren
 let pokemonItem = "";
 let containterRoot = document.getElementById("pokemonList");
 containterRoot.innerHTML= "";
 allPokemon.forEach( element => {
 pokemonItem += `
-
-   <div class="col-sm-12 col-md-6 col-xl-3 col-lg-3">
-      <div class="card" >
-         <div class="card-body">
-            <div class="img">
-               <img src="${element.img}">
-            </div>
-            <span class="cardNum">${element.num}</span>
-            <h2 class="cardName">${element.name}</h2>
-            <p class="cardType">Tipo: ${element.type}</p>
+<div class="col-sm-12 col-md-6 col-xl-3 col-lg-3">
+   <div class="card" >
+      <div class="card-body">
+         <div class="img">
+            <img src="${element.img}">
          </div>
+         <span class="cardNum">${element.num}</span>
+         <h2 class="cardName">${element.name}</h2>
+         <p class="cardType">Tipo: ${element.type}</p>
       </div>
    </div>
 </div>
